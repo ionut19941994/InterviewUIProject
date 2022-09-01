fab58c7eaf56d93faea5211172bf56bdb022b6e8
ionut19941994 <ionut19941994@yahoo.com>
2022-09-01 10:23:27 +0300

    added cucumber.properties modified Homepage steps modified version in pom file added gitignore paths

--- src/test/java/cucumber/StepDefinition/HomepageSteps.java
+++ src/test/java/cucumber/StepDefinition/HomepageSteps.java
@@ -14,8 +14,8 @@ import static org.junit.Assert.assertThat;
 
 public class HomepageSteps{
     public WebDriver driver;
-    Homepage h;
-    CartPage c;
+    Homepage h = new Homepage(driver);
+    CartPage c = new CartPage(driver);
 
 
     @Given("that the user is on the landing page")
@@ -25,52 +25,43 @@ public class HomepageSteps{
 
     @And("he is able to search for {string}")
     public void user_searched_for_keyword(String product) {
-        h=new Homepage(driver);
         h.searchForAProduct(product);
     }
 
     @When("the user clicks on add to basket button")
     public void user_add_to_basket() throws Throwable{
-        h=new Homepage(driver);
         h.addToCart();
     }
 
     @Then("he is able to navigate to the basket page")
     public void user_verifies_the_basket_page() {
-        h=new Homepage(driver);
-        c=new CartPage(driver);
         h.navigateToMyCart();
         assertThat(c.isBasketTitleDisplayed(), is(true));
     }
 
     @Then("he is able to delete an item from the cart")
     public void user_deletes_an_item() throws Throwable{
-        c=new CartPage(driver);
         c.deleteFromCart();
     }
 
     @And("the user will see that the basket was updated with {string}")
     public void user_sees_the_updated_basket(String str){
-        c=new CartPage(driver);
         assertThat(c.getProductName(str).getText().toLowerCase().contains(str.toLowerCase()), is(true));
     }
 
     @And("he sees that the item {string} is not present anymore in the cart")
-    public void user_asserts_the_item_is_no_longer_present(String str) throws Throwable{
-        c=new CartPage(driver);
+    public void user_asserts_the_item_is_no_longer_present(String str) {
         assertThat(c.isProductVisible(str), is(false));
 
     }
 
     @And("he sees that no items are present anymore")
     public void user_asserts_to_no_items_are_present_anymore() {
-        c=new CartPage(driver);
         assertThat(c.pageTitle().getText(),is("Cosul tau este gol"));
     }
 
     @And("he is not able to delete any more item from the cart")
     public void user_asserts_that_user_can_no_longer_delete_items() throws Throwable{
-        c=new CartPage(driver);
         assertThat(c.isDeletePresent(), is(false));
         assertThat(c.isBackToHomeButtonDisplayed(), is(true));
     }
